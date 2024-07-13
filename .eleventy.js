320ba89fc9a4946ca2917c2f0e877af0dc027762
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-07-13 11:25:44 +0700

    base

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,7 @@
 module.exports = function (eleventyConfig) {
-	eleventyConfig.addPassthroughCopy({ 'src/static': '/' });
+	eleventyConfig.addPassthroughCopy({ 'src/assets': '/assets' });
+	eleventyConfig.addPassthroughCopy({ 'src/_includes/styles': '/styles' });
+	eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': '/scripts' });
 	return {
 		dir: {
 			input: 'src',
