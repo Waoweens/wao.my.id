ea7c11231114626f1f511038f65340b7ed5f0ebe
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-07-18 15:12:01 +0700

    new tag load system

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,13 +1,30 @@
 const postcssPlugin = require('@jgarber/eleventy-plugin-postcss');
+const indexes = require('./src/_data/indexes.json');
 
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
+	eleventyConfig.addShortcode('loadIndex', (type) => {
+		console.log(indexes);
+		let tags = [];
+		for (const item of indexes[type]['list']) {
+			const uri = item.startsWith('ROOT/')
+				? item.slice(4)
+				: `/assets/${type}/${item}`;
+			tags.push(indexes[type]['tag'].replace('{uri}', uri));
+		}
+		return tags.join('\n');
+	});
+
 	eleventyConfig.addPassthroughCopy({ 'src/assets': '/assets' });
-	// eleventyConfig.addPassthroughCopy({ 'src/_includes/styles': '/styles' });
-	eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': '/scripts' });
+	eleventyConfig.addPassthroughCopy({
+		'src/_includes/stylesheets': '/assets/stylesheets',
+	});
+	eleventyConfig.addPassthroughCopy({
+		'src/_includes/scripts': '/assets/scripts',
+	});
 
 	eleventyConfig.addPlugin(postcssPlugin);
-	
+
 	return {
 		dir: {
 			input: 'src',
