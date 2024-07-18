f2590cc125ed57f02457ead8ee2be7e7120ba153
Waoweens <30313089+Waoweens@users.noreply.github.com>
2024-07-18 10:45:09 +0700

    eleventy postcss plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,8 +1,13 @@
+const postcssPlugin = require('@jgarber/eleventy-plugin-postcss');
+
 /** @param {import("@11ty/eleventy").UserConfig} eleventyConfig */
 module.exports = function (eleventyConfig) {
 	eleventyConfig.addPassthroughCopy({ 'src/assets': '/assets' });
-	eleventyConfig.addPassthroughCopy({ 'src/_includes/styles': '/styles' });
+	// eleventyConfig.addPassthroughCopy({ 'src/_includes/styles': '/styles' });
 	eleventyConfig.addPassthroughCopy({ 'src/_includes/scripts': '/scripts' });
+
+	eleventyConfig.addPlugin(postcssPlugin);
+	
 	return {
 		dir: {
 			input: 'src',
