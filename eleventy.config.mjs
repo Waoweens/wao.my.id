776f7c5fa3fbcd9fe0908f0b0a2b43f436e63fbd
Waoweens <30313089+Waoweens@users.noreply.github.com>
2025-12-31 12:10:41 +0700

    add plugin syntaxhighlight, add shortode for codeblock, reorder things

--- eleventy.config.mjs
+++ eleventy.config.mjs
@@ -1,10 +1,30 @@
 import postcssPlugin from "@jgarber/eleventy-plugin-postcss";
+import syntaxHighlight from "@11ty/eleventy-plugin-syntaxhighlight";
 import indexes from './src/_data/indexes.json' with { type: 'json' };
+import { createHash } from 'node:crypto';
 
 import 'dotenv/config';
 
 /** @param {import('@11ty/eleventy/UserConfig').default} eleventyConfig*/
 export default async function (eleventyConfig) {
+	eleventyConfig.addPlugin(postcssPlugin);
+	eleventyConfig.addPlugin(syntaxHighlight, {
+		codeAttributes: {
+			id: ({ _, content }) => {
+				return 'codeblock-' + createHash('sha1').update(content).digest('hex').slice(0, 8)
+			}
+		}
+	});
+
+	eleventyConfig.addPairedNunjucksShortcode('codeblock', function(content, language = 'Plain Text') {
+		const id = 'codeblock-' + createHash('sha1').update(content).digest('hex').slice(0, 8)
+		return this.env.render('src/_includes/components/codeblock.njk', { content, language, id });
+	})
+
+	eleventyConfig.setServerOptions({
+		host: "0.0.0.0"
+	})
+
 	eleventyConfig.addShortcode('loadIndex', (type) => {
 		let tags = [];
 		for (const item of indexes[type]['list']) {
@@ -16,16 +36,10 @@ export default async function (eleventyConfig) {
 		return tags.join('\n\t');
 	});
 
-	eleventyConfig.setServerOptions({
-		host: "0.0.0.0"
-	})
-
 	eleventyConfig.addPassthroughCopy({ 'src/assets': '/assets' });
 	// stylesheets copying handled by PostCSS plugin
 	// eleventyConfig.addPassthroughCopy({ 'src/stylesheets': '/stylesheets' });
 	eleventyConfig.addPassthroughCopy({ 'src/scripts': '/scripts' });
-
-	eleventyConfig.addPlugin(postcssPlugin);
 
 };
 
